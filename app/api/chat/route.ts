c6acd506ef6261cbc3ebc976f0b55f01684cc073
aidanolima <aidanolima@gmail.com>
2026-04-14 08:40:57 -0400

    UPDate Lite Version linha 39

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -35,8 +35,8 @@ export async function POST(req: Request) {
     const promptCompleto = `${systemPrompt}\n\nHistórico da conversa:\n${historyText}\n\nAssistente:`;
 
     // Usando o modelo de nova geração que sua chave liberou!
-    const googleURL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
-
+    //const googleURL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+      const googleURL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${apiKey}`;
     const response = await fetch(googleURL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
